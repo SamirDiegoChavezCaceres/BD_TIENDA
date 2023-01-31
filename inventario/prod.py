f437c35679da91a752acbaee5bbbb7adf8850e9c
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2023-01-31 18:12:23 -0500

    setting logs

--- inventario/prod.py
+++ inventario/prod.py
@@ -3,45 +3,47 @@
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io']
 DEBUG = False #cuando es true static se saca de statics, si es falso se saca de collection
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "root": {"level": "ERROR", "handlers": ["file"]},
-    "handlers": {
-        "console": {
-            'class': 'logging.StreamHandler',
-            'formatter': 'app',
+import logging.config
+LOGGING_CONFIG = None
+logging.config.dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "root": {"level": "ERROR", "handlers": ["file"]},
+        "handlers": {
+            "console": {
+                'class': 'logging.StreamHandler',
+                'formatter': 'app',
+            },
         },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "INFO",
-            "propagate": True
+        "loggers": {
+            "django": {
+                "handlers": ["console"],
+                "level": "INFO",
+                "propagate": True
+            },
+            "django.request": {
+                "handlers": ["console"],
+                "level": "INFO",
+                "propagate": True
+            },
+            "DBTienda": {
+                "handlers": ["console"],
+                "level": "INFO",
+                "propagate": True
+            },
+            "menuAndWelcome": {
+                "handlers": ["console"],
+                "level": "INFO",
+                "propagate": True
+            },
         },
-        "django.request": {
-            "handlers": ["fileRequest"],
-            "level": "INFO",
-            "propagate": True
+        "formatters": {
+            "app": {
+                "format": (
+                    u"[%(levelname)s] %(asctime)s "
+                    "(%(module)s.%(funcName)s) %(message)s"
+                ),
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
         },
-        "DBTienda": {
-            "handlers": ["fileDB"],
-            "level": "INFO",
-            "propagate": True
-        },
-        "menuAndWelcome": {
-            "handlers": ["fileMenu"],
-            "level": "INFO",
-            "propagate": True
-        },
-    },
-    "formatters": {
-        "app": {
-            "format": (
-                u"[%(levelname)s] %(asctime)s "
-                "(%(module)s.%(funcName)s) %(message)s"
-            ),
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    },
-}
+    })
