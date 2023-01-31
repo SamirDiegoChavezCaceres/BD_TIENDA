3a4b2137133f638bac88e9639027b6845d6f4cbb
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2023-01-31 18:08:08 -0500

    setting logs

--- inventario/prod.py
+++ inventario/prod.py
@@ -8,29 +8,9 @@ LOGGING = {
     "disable_existing_loggers": False,
     "root": {"level": "ERROR", "handlers": ["file"]},
     "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": "/var/log/django.log",
-            "formatter": "app",
-        },
-        "fileRequest": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "/var/log/django_request.log",
-            "formatter": "app",
-        },
-        "fileDB": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "/var/log/django_db.log",
-            "formatter": "app",
-        },
-        "fileMenu": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": "/var/log/django_menu.log",
-            "formatter": "app",
+        "console": {
+            'class': 'logging.StreamHandler',
+            'formatter': 'app',
         },
     },
     "loggers": {
