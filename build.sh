5689afc38ce3131c8c780c4f5be6df26a1bb74fb
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2023-01-31 23:31:11 -0500

    db error....

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
