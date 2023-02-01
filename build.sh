6c05eb0ec96b2a0af415175ef3914bff5b10151a
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2023-02-01 01:22:49 -0500

    db error

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.get(username='admin').set_password('admin').save()" | python manage.py shell
 echo "from .models import GzzEstadoRegistro; GzzEstadoRegistro.objects.create('A', 'Activo', null)" | python manage.py shell
