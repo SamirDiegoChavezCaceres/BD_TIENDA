a3ab1dc2e18e3245d703bc9aca4bebd785781456
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-03-10 00:18:44 -0500

    property config

--- inventario/settings.py
+++ inventario/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'inventario.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    """ 'default': {
+""" 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     } """
+DATABASES = {
     'default': dj_database_url.config(
         default=config.DB_URI,
         conn_max_age=600,
